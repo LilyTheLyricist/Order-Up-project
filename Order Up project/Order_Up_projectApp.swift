8d2dcc2f0ee995302f09c79242d60672019ade2c
Lily Jimenez <carlosaurus38@gmail.com>
2025-09-22 13:01:14 -0700

    Initial Commit

---
+++ Order Up project/Order_Up_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Order_Up_projectApp.swift
+//  Order Up project
+//
+//  Created by Jimenez, Carlos - Student on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Order_Up_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Order Up projectTests/Order_Up_projectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Order_Up_projectTests.swift
+//  Order Up projectTests
+//
+//  Created by Jimenez, Carlos - Student on 9/22/25.
+//
+
+import Testing
+@testable import Order_Up_project
+
+struct Order_Up_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
